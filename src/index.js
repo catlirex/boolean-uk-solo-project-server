c0175c204371cd9b69d853e30d9b41b5dc5ad42a
catherine-swyvl <catherine.uk.20@gmail.com>
2021-09-29 12:33:35 +0100

    update: deploy setting

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
